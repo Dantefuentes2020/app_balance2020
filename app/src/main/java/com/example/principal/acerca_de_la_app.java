368bf1d9eacb4caed2b084941e3e914bc409c317
dante.fuentes2019@twk.cl <danteleganster15>
2020-06-03 20:26:25 -0400

    menu

---
+++ app/src/main/java/com/example/principal/acerca_de_la_app.java
@@ -0,0 +1,14 @@
+package com.example.principal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class acerca_de_la_app extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acerca_de_la_app);
+    }
+}
