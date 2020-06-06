cb77a166ea9e8fbdd5e740b3be991998bf8fa0b1
dante.fuentes2019@twk.cl <danteleganster15>
2020-06-06 18:48:39 -0400

    Update Clases & activities

---
+++ app/src/main/java/com/example/principal/bienvenida.java
@@ -0,0 +1,15 @@
+package com.example.principal;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class bienvenida extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenida);
+    }
+}
